3603ecfbff274313edf792d507a0a6f7cf74df97
Trinath Ladi <laditrinath321@gmail.com>
2025-06-30 20:36:30 +0530

    Update backend.tf

--- eks/backend.tf
+++ eks/backend.tf
@@ -10,7 +10,7 @@ terraform {
     bucket         = "dev-trinath-tf-bucket"
     region         = "us-east-1"
     key            = "eks/terraform.tfstate"
-    dynamodb_table = "dev-trinath-tf-bucket"
+    dynamodb_table = "dev-trinath-tf-locks"
     encrypt        = true
   }
 }
